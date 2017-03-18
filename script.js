b6e3b845e9c445c6d3515dcce0729bfda6ae3176
igicruz <ialderete12@gmail.com>
2017-03-17 23:38:14 -0700

    add file
    add file

---
+++ script.js
@@ -0,0 +1,9 @@
+/*JS*/
+var name = "Ignacio";
+var age = 41;
+var dateOfBirth = "Jun 22, 1976";
+var placeOfBirth = "Mexico City";
+
+document.getElementById("demo").innerHTML = name + "<br>" + age + "<br>" 
++ dateOfBirth + "<br>" + placeOfBirth;
+
